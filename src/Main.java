5ea852eca17cd0b26e14c2c5ae14a31ff5ac63d5
septiar <septiarmustafa@gmail.com>
2023-11-14 19:43:16 +0700

    [FEATURE] Challenge - 8.5 Mencari angka minus terbesar yang hilang

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(findMin(new Integer[] {4, 3, 2, 1, -1, -3, -4}));
+    }
+
+    public static Integer findMin(Integer[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                boolean contains = false;
+                for (int n : arr) {
+                    if (n == -arr[i]) {
+                        contains = true;
+                    }
+                }
+                if (!contains && arr[i] > 0) {
+                    return -arr[i];
+                }
+            }
+        }
+        return 0;
     }
 }

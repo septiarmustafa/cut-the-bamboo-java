2a84fc8090ee16b04683cf1817184648aac1eade
baskara <baskararw10@gmail.com>
2023-11-14 20:20:48 +0700

    [FEATURE] Cut The Bamboo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        cutBamboos(input);
+    }
+
+    public static void cutBamboos(Scanner input) {
+        System.out.print("bamboos: ");
+        String bamboos = input.nextLine();
+        System.out.print("cuts: ");
+        Integer cycleCuts = input.nextInt();
+
+        String[] arrBamboos = bamboos.split(",");
+        Integer[] intArrBamboos = new Integer[arrBamboos.length];
+
+        for (int i = 0; i < arrBamboos.length; i++) {
+            intArrBamboos[i] = Integer.parseInt(arrBamboos[i].trim());
+        }
+        System.out.println("initials:");
+        loopingBamboos(intArrBamboos);
+        Integer index=1;
+        do {
+            for (int i = 0; i < intArrBamboos.length; i++) {
+                intArrBamboos[i] = intArrBamboos[i] - 1;
+            }
+            System.out.println("cycle cuts: "+ index++);
+            loopingBamboos(intArrBamboos);
+
+            cycleCuts--;
+        } while (cycleCuts > 0);
+    }
+
+    public static void loopingBamboos (Integer [] intArrBamboos){
+        for (int i = 0; i < intArrBamboos.length; i++) {
+            System.out.print("|");
+            for (int j = 0; j < intArrBamboos[i]; j++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
